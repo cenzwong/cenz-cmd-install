2566dedf8386ca18f1f38f144fa29f21885578d2
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-08-31 11:56:52 +0800

    Create install-mosquito-all.sh

---
+++ MQTT/install-mosquito-all.sh
@@ -0,0 +1,6 @@
+echo "This script is used to install MQTT server/broker"
+echo "Reference site: https://mosquitto.org/download/"
+sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+sudo apt update -y
+sudo apt install -y mosquitto mosquitto-clients
+echo "Basic Testing Command: https://telegra.ph/MQTT-Notes-code-07-14"
