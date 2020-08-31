0470f849dfeee372fa66a64a68078993de1456ab
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-08-31 11:55:58 +0800

    Update install-mosquito-clients.sh

--- MQTT/install-mosquito-clients.sh
+++ MQTT/install-mosquito-clients.sh
@@ -1,1 +1,4 @@
-#
+echo "This script is used to install MQTT client"
+echo "Ref: https://oranwind.org/-edge-zai-ubuntu-an-zhuang-mosquitto-mqtt-broker-part-2/"
+sudo apt install -y mosquitto-clients
+echo "Basic Testing Command: https://telegra.ph/MQTT-Notes-code-07-14"
