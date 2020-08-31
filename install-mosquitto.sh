be6cccea14982c7de7733f36c1d91bcb6a6b81bd
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-08-31 11:51:41 +0800

    Update install-mosquitto.sh

--- install-mosquitto.sh
+++ install-mosquitto.sh
@@ -3,3 +3,4 @@ echo "Reference site: https://mosquitto.org/download/"
 sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
 sudo apt update -y
 sudo apt install -y mosquitto
+echo "Basic Testing Command: https://telegra.ph/MQTT-Notes-code-07-14"
