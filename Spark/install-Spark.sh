dec1636b3d0922a9a5285a0311755ce706baf37b
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-09-27 18:36:54 +0800

    Update install-Spark.sh

--- Spark/install-Spark.sh
+++ Spark/install-Spark.sh
@@ -13,3 +13,14 @@ source ~/.bashrc
 
 cd spark-x
 ./bin/pyspark
+
+# Install Jupyter
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+
+bash ./Anaconda3-2020.07-Linux-x86_64.sh
+nano ~/.bashrc
+>> export PYSPARK_DRIVER_PYTHON="jupyter"
+>> export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+source ~/.bashrc
+
+./bin/pyspark
