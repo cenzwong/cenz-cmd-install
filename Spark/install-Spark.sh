104d81d09c73bbc91ba4ff1a8a27197219a43858
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2020-09-27 18:30:47 +0800

    Update install-Spark.sh

--- Spark/install-Spark.sh
+++ Spark/install-Spark.sh
@@ -1,1 +1,15 @@
 wget http://ftp.cuhk.edu.hk/pub/packages/apache.org/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz
+tar xf spark-x.tgz
+
+
+java -version
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt update
+sudo apt install openjdk-8-jdk
+
+nano ~/.bashrc
+>> export PYSPARK_PYTHON=python3
+source ~/.bashrc
+
+cd spark-x
+./bin/pyspark
