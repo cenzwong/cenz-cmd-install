fdbb09847b0e9f70df8815776f8725fac6df634a
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-01-04 14:44:30 +0800

    Update install.sh

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,3 +1,5 @@
+echo "reference: https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/SingleCluster.html#Standalone_Operation"
+
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install ssh
 sudo apt-get install pdsh
@@ -7,3 +9,7 @@ export JAVA_HOME=/usr/java/latest
 java -version
 echo "java -version"
 pause
+
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt update
+sudo apt install openjdk-8-jdk
