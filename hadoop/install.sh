d828725cc26b281c26365f90c5c7a98afc259040
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-01-04 15:49:49 +0800

    Update install.sh

--- hadoop/install.sh
+++ hadoop/install.sh
@@ -1,8 +1,8 @@
 echo "reference: https://hadoop.apache.org/docs/current/hadoop-project-dist/hadoop-common/SingleCluster.html#Standalone_Operation"
 
 sudo apt update && sudo apt upgrade -y
-sudo apt-get install ssh
-sudo apt-get install pdsh
+sudo apt install openjdk-8-jre-headless # 8 is needed: https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions
+sudo apt install ssh pdsh
 
 # set to the root of your Java installation
 export JAVA_HOME=/usr/java/latest
@@ -10,8 +10,7 @@ java -version
 echo "java -version"
 pause
 
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt update
-sudo apt install openjdk-8-jdk
-
+#
+wget https://mirror-hk.koddos.net/apache/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
+tar -tvf hadoop-3.3.0.tar.gz
 echo "bin/hadoop"
