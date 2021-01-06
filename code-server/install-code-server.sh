fcf031dd3c05bf566a1787177e6553fb9c878118
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-01-06 16:52:34 +0800

    Update install-code-server.sh

--- code-server/install-code-server.sh
+++ code-server/install-code-server.sh
@@ -1,1 +1,7 @@
+curl -fsSL https://code-server.dev/install.sh | sh
 
+# or
+curl -fOL https://github.com/cdr/code-server/releases/download/v3.8.0/code-server_3.8.0_amd64.deb
+sudo dpkg -i code-server_3.8.0_amd64.deb
+sudo systemctl enable --now code-server@$USER
+# Now visit http://127.0.0.1:8080. Your password is in ~/.config/code-server/config.yaml
