d9ef71e711e9af06e9bec4442cb071217c5cb873
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-01-21 00:10:33 +0800

    Create install.sh

---
+++ dlib/install.sh
@@ -0,0 +1,9 @@
+# https://www.pyimagesearch.com/2018/01/22/install-dlib-easy-complete-guide/
+
+
+sudo apt-get update
+sudo apt-get install build-essential cmake
+sudo apt-get install libopenblas-dev liblapack-dev 
+sudo apt-get install libx11-dev libgtk-3-dev
+
+pip install dlib
