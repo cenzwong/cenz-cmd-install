041b45c30493766c23dd9de7c3a036cc4c9288f5
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-01-27 11:55:40 +0800

    Update install.sh

--- dlib/install.sh
+++ dlib/install.sh
@@ -2,8 +2,8 @@
 
 
 sudo apt-get update
-sudo apt-get install build-essential cmake
-sudo apt-get install libopenblas-dev liblapack-dev 
-sudo apt-get install libx11-dev libgtk-3-dev
+sudo apt-get install build-essential cmake -y
+sudo apt-get install libopenblas-dev liblapack-dev -y 
+sudo apt-get install libx11-dev libgtk-3-dev -y
 
 pip install dlib
