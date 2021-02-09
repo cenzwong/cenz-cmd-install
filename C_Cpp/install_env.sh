bd94db9dc521471170de99b9e89d879030a34a7b
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-02-09 11:40:01 +0800

    Update install_env.sh

--- C_Cpp/install_env.sh
+++ C_Cpp/install_env.sh
@@ -1,3 +1,10 @@
 # Preparing C compiler (gcc is the C compiler)
 sudo apt install gcc
+# or
+sudo apt install build-essential
+
+# Preparing C++ compiler (g++ is the C compiler)
+sudo apt install g++
+# or
+$ sudo apt install build-essential
 
