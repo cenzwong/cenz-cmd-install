772fab213734a4ee20ed3ce286e08760c0e2b3b2
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-02-09 11:48:49 +0800

    Create Cpp_Helloworld.cpp

---
+++ C_Cpp/Cpp_Helloworld.cpp
@@ -0,0 +1,13 @@
+// Cpp Helloworld program
+// g++ -o hello Cpp_Helloworld.cpp
+// ./hello 
+
+#include <iostream>
+//using namespace std;
+
+int main() 
+{
+    // Stream text to standard output, and stream end line
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
