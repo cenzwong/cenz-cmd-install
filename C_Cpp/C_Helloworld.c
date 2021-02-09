0821ee1522933fa11414b8f810e5ed404f722c59
Cenz Wong <44856918+cenzwong@users.noreply.github.com>
2021-02-09 11:37:31 +0800

    Create C_Helloworld.c

---
+++ C_Cpp/C_Helloworld.c
@@ -0,0 +1,13 @@
+// C Helloworld
+// To Compile the .c file
+// gcc -o hello C_Helloworld.c 
+//
+// To Run the C Program
+// ./hello 
+
+#include <stdio.h>
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
